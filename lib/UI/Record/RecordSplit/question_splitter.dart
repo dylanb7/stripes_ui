bb33146fdf67bdc3681d5cda1ebef6cdd5392b98
Dylan Baker <dylanbaker7@gmail.com>
2023-12-11 21:27:04 -0500

    d

--- lib/UI/Record/RecordSplit/question_splitter.dart
+++ lib/UI/Record/RecordSplit/question_splitter.dart
@@ -94,7 +94,7 @@ final checkinProvider =
       ret[searchPeriod]!.add(CheckinItem(
           path: byType.value,
           type: byType.key,
-          response: valid.isEmpty ? null : valid.first as Response));
+          response: valid.isEmpty ? null : valid.first as DetailResponse));
     }
   }
   return ret;
@@ -104,7 +104,7 @@ final checkinProvider =
 class CheckinItem {
   final RecordPath path;
   final String type;
-  final Response? response;
+  final DetailResponse? response;
 
   const CheckinItem(
       {required this.path, required this.type, required this.response});
