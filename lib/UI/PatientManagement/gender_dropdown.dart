2ce9c619129f0d53280c61a59595f2181558b4a0
Dylan Baker <dylanbaker7@gmail.com>
2024-02-29 21:23:54 -0500

    a

--- lib/UI/PatientManagement/gender_dropdown.dart
+++ lib/UI/PatientManagement/gender_dropdown.dart
@@ -24,44 +24,17 @@ class GenderDropdown extends FormField<String> {
             initialValue: initialValue,
             builder: (FormFieldState<String> state) {
               final List<String> values = ["Male", "Female", "Other"];
-              return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 0, horizontal: 0),
-                      decoration: BoxDecoration(
-                          border: state.errorText == null
-                              ? Border.all(
-                                  color: Theme.of(context).colorScheme.primary)
-                              : Border.all(
-                                  color: Theme.of(context).colorScheme.error),
-                          borderRadius: BorderRadius.circular(5)),
-                      child: DropdownMenu<String>(
-                        onSelected: (value) {
-                          holder.gender = value;
-                          state.didChange(value);
-                        },
-                        initialSelection: state.value,
-                        trailingIcon: const Icon(
-                          Icons.arrow_downward,
-                        ),
-                        dropdownMenuEntries: values
-                            .map((e) => DropdownMenuEntry(value: e, label: e))
-                            .toList(),
-                        errorText: state.errorText,
-                      ),
-                    ),
-                    if (state.hasError)
-                      Padding(
-                          padding: const EdgeInsets.only(left: 14.0, top: 6.0),
-                          child: Text(
-                            state.errorText!,
-                            textAlign: TextAlign.left,
-                            style: lightBackgroundStyle.copyWith(
-                                fontSize: 14.0,
-                                color: Theme.of(context).colorScheme.error),
-                          )),
-                  ]);
+              return DropdownMenu<String>(
+                onSelected: (value) {
+                  holder.gender = value;
+                  state.didChange(value);
+                },
+                hintText: "Gender",
+                initialSelection: state.value,
+                dropdownMenuEntries: values
+                    .map((e) => DropdownMenuEntry(value: e, label: e))
+                    .toList(),
+                errorText: state.errorText,
+              );
             });
 }
