4ecd2935f8286b24068dba0c4b094719bd12e740
Dylan Baker <dylanbaker7@gmail.com>
2024-03-21 15:25:41 -0400

    d

--- lib/Util/easy_snack.dart
+++ lib/Util/easy_snack.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:stripes_ui/l10n/app_localizations.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
 showSnack(BuildContext context, String message,
     {Function? action, String? actionMessage}) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    content: Text(message),
-    dismissDirection: DismissDirection.up,
-    behavior: SnackBarBehavior.floating,
-    margin: EdgeInsets.only(
-        left: 10, right: 10, bottom: MediaQuery.of(context).size.height - 100),
-    action: action != null
-        ? SnackBarAction(
-            label: actionMessage ?? AppLocalizations.of(context)!.undo,
-            onPressed: () {
-              action();
-            })
-        : null,
-  ));
+  showTopSnackBar(
+      Overlay.of(context),
+      SnackBar(
+        content: Text(message),
+        dismissDirection: DismissDirection.up,
+        behavior: SnackBarBehavior.floating,
+        margin: EdgeInsets.only(
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).size.height - 100),
+        action: action != null
+            ? SnackBarAction(
+                label: actionMessage ?? AppLocalizations.of(context)!.undo,
+                onPressed: () {
+                  action();
+                })
+            : null,
+      ));
 }
