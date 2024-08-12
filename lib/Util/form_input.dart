6595e4cfef22593b241c49386584585060f6ba58
Dylan Baker <dylanbaker7@gmail.com>
2024-08-12 14:29:28 -0400

    k

--- lib/Util/form_input.dart
+++ lib/Util/form_input.dart
@@ -11,22 +11,17 @@ InputDecoration formFieldDecoration(
     prefixIcon: prefix == null ? null : Icon(prefix),
     suffixIcon: suffix ??
         (clearable
-            ? ListenableBuilder(
-                listenable: controller,
-                builder: (context, child) {
-                  if (controller.text.isEmpty) return Container();
-                  return IconButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {
-                      controller.clear();
-                    },
-                    icon: const Icon(
-                      Icons.clear,
-                    ),
-                  );
-                })
+            ? IconButton(
+                padding: EdgeInsets.zero,
+                onPressed: () {
+                  controller.clear();
+                },
+                icon: const Icon(
+                  Icons.clear,
+                ),
+              )
             : null),
-    floatingLabelBehavior: FloatingLabelBehavior.auto,
+    floatingLabelBehavior: FloatingLabelBehavior.always,
     hintText: hintText,
     labelText: hintText,
     errorText: errorText,
