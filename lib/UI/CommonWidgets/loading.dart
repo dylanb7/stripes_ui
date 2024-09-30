f95693ed8cf9f2308906bbc9a189efb43c46d041
Dylan Baker <dylanbaker7@gmail.com>
2024-09-30 16:15:33 -0400

    d

--- lib/UI/CommonWidgets/loading.dart
+++ lib/UI/CommonWidgets/loading.dart
@@ -6,14 +6,12 @@ class LoadingWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: ColoredBox(
-        color: Theme.of(context).canvasColor,
-        child: Center(
-          child: LoadingAnimationWidget.staggeredDotsWave(
-              color: Theme.of(context).primaryColor,
-              size: Theme.of(context).iconTheme.size ?? 40),
-        ),
+    return ColoredBox(
+      color: Theme.of(context).canvasColor,
+      child: Center(
+        child: LoadingAnimationWidget.staggeredDotsWave(
+            color: Theme.of(context).primaryColor,
+            size: Theme.of(context).iconTheme.size ?? 40),
       ),
     );
   }
