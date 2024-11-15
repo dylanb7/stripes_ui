eb94b2a8b02f081a4eb7bfa108523cfe15435335
Dylan Baker <dylanbaker7@gmail.com>
2024-11-14 20:11:30 -0500

    f

--- lib/UI/Record/TestScreen/recordings_state.dart
+++ lib/UI/Record/TestScreen/recordings_state.dart
@@ -127,7 +127,7 @@ class _RecordingsWaitingState extends ConsumerState<RecordingsWaiting> {
 
   Timer? timer;
 
-  final Duration waitTime = const Duration(seconds: 30);
+  final Duration waitTime = const Duration(days: 6);
 
   @override
   void didChangeDependencies() {
