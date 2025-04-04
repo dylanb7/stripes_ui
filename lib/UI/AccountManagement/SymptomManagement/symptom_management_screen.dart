186694658a8aa614ec8e7b06d2eed97fdb98e0be
Dylan Baker <dylanbaker7@gmail.com>
2025-04-04 15:52:03 -0400

    f

--- lib/UI/AccountManagement/SymptomManagement/symptom_management_screen.dart
+++ lib/UI/AccountManagement/SymptomManagement/symptom_management_screen.dart
@@ -3,8 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:stripes_backend_helper/RepositoryBase/QuestionBase/question_repo_base.dart';
 import 'package:stripes_backend_helper/RepositoryBase/QuestionBase/record_period.dart';
+import 'package:stripes_ui/UI/CommonWidgets/user_profile_button.dart';
+import 'package:stripes_ui/UI/Layout/home_screen.dart';
 import 'package:stripes_ui/UI/Layout/tab_view.dart';
 import 'package:stripes_ui/UI/Record/RecordSplit/question_splitter.dart';
+import 'package:stripes_ui/Util/breakpoint.dart';
 import 'package:stripes_ui/Util/constants.dart';
 
 class SymptomManagementScreen extends ConsumerWidget {
@@ -16,54 +19,68 @@ class SymptomManagementScreen extends ConsumerWidget {
         ref.watch(recordProvider(PageProps(context: context)));
     final Map<Period, List<CheckinItem>> checkin =
         ref.watch(checkinProvider(CheckInProps(context: context)));
+    final bool isSmall = getBreakpoint(context).isLessThan(Breakpoint.medium);
 
-    return RefreshWidget(
-      depth: RefreshDepth.subuser,
-      scrollable: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(
-              height: 20.0,
-            ),
-            Row(
-              children: [
-                IconButton(
-                    onPressed: () {
-                      context.goNamed(Routes.ACCOUNT);
-                    },
-                    icon: const Icon(Icons.keyboard_arrow_left)),
-                Text(
-                  'Symptoms',
-                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).primaryColor),
-                  textAlign: TextAlign.left,
-                ),
-                const Spacer(),
-              ],
-            ),
-            const SizedBox(
-              height: 6.0,
-            ),
-            const Text("Categories"),
-            ...recordPaths.keys.map((key) {
-              final RecordPath path = recordPaths[key]!;
-              final int symptoms = path.pages.isEmpty ? 0 : path.pages.map((page) => page.questionIds.length).reduce((value, element) => value + element)
-              return ListTile(
-                title: Text(key),
-                subtitle: Text(
-                    "Symptoms: $symptoms}"),
-              );
-            }),
-            /*
+    return PageWrap(
+      actions: [
+        if (!isSmall)
+          ...TabOption.values.map((tab) => LargeNavButton(tab: tab)),
+        const UserProfileButton(
+          selected: true,
+        )
+      ],
+      bottomNav: isSmall ? const SmallLayout() : null,
+      child: RefreshWidget(
+        depth: RefreshDepth.subuser,
+        scrollable: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        context.goNamed(Routes.ACCOUNT);
+                      },
+                      icon: const Icon(Icons.keyboard_arrow_left)),
+                  Text(
+                    'Symptoms',
+                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).primaryColor),
+                    textAlign: TextAlign.left,
+                  ),
+                  const Spacer(),
+                ],
+              ),
+              const SizedBox(
+                height: 6.0,
+              ),
+              const Text("Categories"),
+              ...recordPaths.keys.map((key) {
+                final RecordPath path = recordPaths[key]!;
+                final int symptoms = path.pages.isEmpty
+                    ? 0
+                    : path.pages
+                        .map((page) => page.questionIds.length)
+                        .reduce((value, element) => value + element);
+                return ListTile(
+                  title: Text(key),
+                  subtitle: Text("Symptoms: $symptoms}"),
+                );
+              }),
+              /*
             ...checkin.keys.map((items) {
               items.map((item) => {
                 item.
               });
               return Container();
             })*/
-          ],
+            ],
+          ),
         ),
       ),
     );
