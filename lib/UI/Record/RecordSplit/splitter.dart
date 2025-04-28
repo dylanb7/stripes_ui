fc5707788eff7f58f20c7ab0e2c045178149b60b
Dylan Baker <dylanbaker7@gmail.com>
2025-04-28 07:59:41 -0700

    r

--- lib/UI/Record/RecordSplit/splitter.dart
+++ lib/UI/Record/RecordSplit/splitter.dart
@@ -74,12 +74,13 @@ class RecordSplitterState extends ConsumerState<RecordSplitter> {
   Widget build(BuildContext context) {
     final PageProps props =
         PageProps(context: context, questionListener: widget.questionListener);
-    final List<PageLayout> pages = ref
-            .watch(pagePaths(props))[widget.type]
-            ?.pages
-            .where((page) => page.dependsOn.eval(widget.questionListener))
-            .toList() ??
-        [];
+    final List<PageLayout> unfiltteredPages =
+        ref.watch(pagePaths(props))[widget.type]?.pages ?? [];
+
+    final List<PageLayout> pages = unfiltteredPages
+        .where((page) => page.dependsOn.eval(widget.questionListener))
+        .toList();
+
     final bool isSmall = getBreakpoint(context).isLessThan(Breakpoint.medium);
 
     final AsyncValue<QuestionRepo> questionRepo = ref.watch(questionsProvider);
