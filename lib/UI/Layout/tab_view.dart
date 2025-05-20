83e717731e62ca3face2a30990ecd5b31e4693e1
Dylan Baker <dylanbaker7@gmail.com>
2025-05-19 18:58:28 -0700

    k

--- lib/UI/Layout/tab_view.dart
+++ lib/UI/Layout/tab_view.dart
@@ -131,25 +131,25 @@ class HistoryScreenContent extends ConsumerWidget {
                       ),
                     ),
                   ),
-                  SliverConstrainedCrossAxis(
-                    maxExtent: Breakpoint.small.value,
-                    sliver: SliverPadding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      sliver: SliverList(
-                        delegate: SliverChildListDelegate(
-                          const [
-                            FilterView(),
-                            SizedBox(
-                              height: 8.0,
-                            ),
-                            AutomaticKeepAlive(
-                              child: EventsCalendar(),
-                            ),
-                            SizedBox(
-                              height: 16.0,
-                            ),
-                            ActionRow()
-                          ],
+                  AutomaticKeepAlive(
+                    child: SliverConstrainedCrossAxis(
+                      maxExtent: Breakpoint.small.value,
+                      sliver: SliverPadding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        sliver: SliverList(
+                          delegate: SliverChildListDelegate(
+                            const [
+                              FilterView(),
+                              SizedBox(
+                                height: 8.0,
+                              ),
+                              EventsCalendar(),
+                              SizedBox(
+                                height: 16.0,
+                              ),
+                              ActionRow()
+                            ],
+                          ),
                         ),
                       ),
                     ),
