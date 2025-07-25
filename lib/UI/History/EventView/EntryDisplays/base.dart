1b87f81cf6bbf45314c8899817faa5f63fe6d761
Dylan Baker <dylanbaker7@gmail.com>
2025-07-24 21:11:04 -0400

    j

--- lib/UI/History/EventView/EntryDisplays/base.dart
+++ lib/UI/History/EventView/EntryDisplays/base.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:collection/collection.dart';
 import 'package:duration/duration.dart';
 import 'package:duration/locale.dart';
