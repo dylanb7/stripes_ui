49bf21702c09612b8be82e823dfd84798d66936a
Dylan Baker <dylanbaker7@gmail.com>
2026-01-21 13:20:41 -0500

    waiting duration back to 7 days

--- lib/UI/Record/TestScreen/recordings_state.dart
+++ lib/UI/Record/TestScreen/recordings_state.dart
@@ -167,7 +167,7 @@ class _WaitingTimeState extends ConsumerState<WaitingTime> {
 
   Timer? timer;
 
-  final Duration waitTime = const Duration(seconds: 30);
+  final Duration waitTime = const Duration(days: 7);
 
   @override
   Widget build(BuildContext context) {
